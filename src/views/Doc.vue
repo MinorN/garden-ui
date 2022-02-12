faac9371fa2957f84ca4cd70fd0855b0a9a3f79f
MinorN <1481700559@qq.com>
2022-02-13 00:48:13 +0800

    使用provide和inject实现aside切换

--- src/views/Doc.vue
+++ src/views/Doc.vue
@@ -3,7 +3,7 @@
     <topnav/>
     <div>
       <div class="content">
-        <aside>
+        <aside v-if="menuVisible">
           <h2>组件列表</h2>
           <ol>
             <li>
@@ -27,10 +27,15 @@
 </template>
 
 
-<script>
+<script lang="ts">
 import Topnav from "../components/Topnav.vue";
+import {inject, Ref} from "vue";
 export default {
-  components: {Topnav}
+  components: {Topnav},
+  setup(){
+    const menuVisible = inject<Ref<boolean>>('menuVisible')
+    return{menuVisible}
+  }
 }
 </script>
 <style lang="scss" scoped>
