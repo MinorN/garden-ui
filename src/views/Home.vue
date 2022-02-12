9d64bea542b7140f5c360f2617a3d9d0d0c95073
MinorN <1481700559@qq.com>
2022-02-13 00:05:01 +0800

    topnav样式更新

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,22 +1,39 @@
 <template>
   <div>
     <div class="topnav">
-      <div class="logo"></div>
-      <div class="menu"></div>
+      <div class="logo">LOGO</div>
+       <ul class="menu">
+         <li>菜单1</li>
+         <li>菜单2</li>
+       </ul>
     </div>
     <div class="banner">
-      <h1>花园ui</h1>
+      <h1>花园UI</h1>
       <h2>一个厉害的UI框架</h2>
-      <p class="actions">
-        <a href="">GitHub</a>
-        <a href="">开始</a>
-      </p>
+      <a href="">GitHub</a>
+      <a href="">开始</a>
     </div>
-
   </div>
 </template>
 
 
 
 <style lang="scss" scoped>
+.topnav {
+  background: pink;
+  display: flex;
+  padding: 16px;
+  > .logo {
+    max-width: 6em;
+    margin-right: auto;
+  }
+  > .menu {
+    display: flex;
+    white-space: nowrap;
+    flex-wrap: nowrap;
+    > li {
+      margin: 0 1em;
+    }
+  }
+}
 </style>
