d07dac79b67ca852acd96b3f5fd057b1a43a7a28
MinorN <1481700559@qq.com>
2022-02-14 00:26:05 +0800

    添加theme属性

--- src/lib/Button.vue
+++ src/lib/Button.vue
@@ -1,11 +1,20 @@
 <template>
-  <button>
-    <slot/>
-  </button>
+    <button class="garden-button"
+    :class="{[`theme-${theme}`]:theme}">
+      <slot/>
+    </button>
 </template>
 
 <script lang="ts">
+export default {
+  props:{
+    theme:{
+      type:String,
+      default:'button'
+    }
 
+  }
+};
 </script>
 
 <style lang="scss" scoped>
