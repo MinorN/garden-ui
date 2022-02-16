ad2ac4bee626b6338878b839be3f61b8f033d59d
MinorN <1481700559@qq.com>
2022-02-16 20:01:43 +0800

    引入图标

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,22 +1,45 @@
 <template>
-  <div class="topnavAndBanner">
-    <Topnav/>
-    <div class="banner">
-      <h1>花园UI</h1>
-      <h2>一个厉害的UI框架</h2>
-      <p class="actions">
-        <a href="https://github.com/MinorN/huayuan-ui">GitHub</a>
-        <router-link to="/doc">开始</router-link>
-      </p>
+  <div>
+    <div class="topnavAndBanner">
+      <Topnav/>
+      <div class="banner">
+        <h1>花园UI</h1>
+        <h2>一个厉害的UI框架</h2>
+        <p class="actions">
+          <a href="https://github.com/MinorN/huayuan-ui">GitHub</a>
+          <router-link to="/doc">开始</router-link>
+        </p>
+      </div>
+    </div>
+    <div class="features">
+      <svg class="icon" aria-hidden="true">
+        <use xlink:href="#icon-vue"></use>
+      </svg>
+      <svg class="icon" aria-hidden="true">
+        <use xlink:href="#icon-typescript"></use>
+      </svg>
+      <svg class="icon" aria-hidden="true">
+        <use xlink:href="#icon-light"></use>
+      </svg>
     </div>
   </div>
 </template>
 
 
 <style lang="scss" scoped>
-.topnavAndBanner{
-  background: rgb(227,255,253);
-  background: linear-gradient(145deg, rgba(227,255,253,1) 0%, rgba(183,233,230,1) 100%);
+$green: #02bcb0;
+$border-radius: 4px;
+$color: #007974;
+.topnavAndBanner {
+  background: rgb(227, 255, 253);
+  background: linear-gradient(145deg, rgba(227, 255, 253, 1) 0%, rgba(183, 233, 230, 1) 100%);
+}
+
+.features{
+  >svg{
+    width: 64px;
+    height: 64px;
+  }
 }
 
 .banner {
@@ -25,23 +48,29 @@
   justify-content: center;
   align-items: center;
   flex-direction: column;
-  > .actions{
+  color: $color;
+
+  > .actions {
     padding: 8px 0;
+
     a {
-      margin:0 8px;
-      background: #fff;
+      margin: 0 8px;
+      background: $green;
+      color: white;
       display: inline-block;
-      $h: 28px;
-      height: $h;
-      line-height: $h;
-      border-radius: $h/2;
-      padding: 0 8px;
+      padding: 8px 24px;
+      border-radius: $border-radius;
+
+      &:hover {
+        text-decoration: none;
+      }
     }
   }
 }
 </style>
 <script>
 import Topnav from "../components/Topnav.vue";
+
 export default {
   components: {Topnav}
 }
